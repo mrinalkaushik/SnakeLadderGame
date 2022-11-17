7deabbe2d77daa5871c4e41f9b7044ae11bd5f62
mrinal kaushik <mrinalrocks24x7gmail.com>
2022-11-17 11:53:19 +0530

    ADD: player roll dice to get number between 1 and 6

---
+++ Snakeladdergame.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.snakeladder;
+
+public class Snakeladdergame {
+	public static void main(String[] arg) {
+		System.out.println("Welcome to snake lader game");
+		System.out.println("Roll the dice");
+		Player.rolldice();
+	}
+}
